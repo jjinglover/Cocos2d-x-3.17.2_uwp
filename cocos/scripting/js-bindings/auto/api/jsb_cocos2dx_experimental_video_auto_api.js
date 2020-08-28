a4472024b1a724f81543ad3ff5aae75c3040c7e4
jjinglover <jjinglover@163.com>
2020-08-28 23:02:22 +0800

    add original Cocos2d-x-3.17.2

---
+++ cocos/scripting/js-bindings/auto/api/jsb_cocos2dx_experimental_video_auto_api.js
@@ -0,0 +1,206 @@
+/**
+ * @module cocos2dx_experimental_video
+ */
+var ccui = ccui || {};
+
+/**
+ * @class VideoPlayer
+ */
+ccui.VideoPlayer = {
+
+/**
+ * @method getFileName
+ * @return {String}
+ */
+getFileName : function (
+)
+{
+    return ;
+},
+
+/**
+ * @method getURL
+ * @return {String}
+ */
+getURL : function (
+)
+{
+    return ;
+},
+
+/**
+ * @method play
+ */
+play : function (
+)
+{
+},
+
+/**
+ * @method isUserInputEnabled
+ * @return {bool}
+ */
+isUserInputEnabled : function (
+)
+{
+    return false;
+},
+
+/**
+ * @method setKeepAspectRatioEnabled
+ * @param {bool} arg0
+ */
+setKeepAspectRatioEnabled : function (
+bool 
+)
+{
+},
+
+/**
+ * @method stop
+ */
+stop : function (
+)
+{
+},
+
+/**
+ * @method setFullScreenEnabled
+ * @param {bool} arg0
+ */
+setFullScreenEnabled : function (
+bool 
+)
+{
+},
+
+/**
+ * @method setFileName
+ * @param {String} arg0
+ */
+setFileName : function (
+str 
+)
+{
+},
+
+/**
+ * @method setURL
+ * @param {String} arg0
+ */
+setURL : function (
+str 
+)
+{
+},
+
+/**
+ * @method setStyle
+ * @param {cc.experimental::ui::VideoPlayer::StyleType} arg0
+ */
+setStyle : function (
+styletype 
+)
+{
+},
+
+/**
+ * @method seekTo
+ * @param {float} arg0
+ */
+seekTo : function (
+float 
+)
+{
+},
+
+/**
+ * @method isKeepAspectRatioEnabled
+ * @return {bool}
+ */
+isKeepAspectRatioEnabled : function (
+)
+{
+    return false;
+},
+
+/**
+ * @method onPlayEvent
+ * @param {int} arg0
+ */
+onPlayEvent : function (
+int 
+)
+{
+},
+
+/**
+ * @method isFullScreenEnabled
+ * @return {bool}
+ */
+isFullScreenEnabled : function (
+)
+{
+    return false;
+},
+
+/**
+ * @method isLooping
+ * @return {bool}
+ */
+isLooping : function (
+)
+{
+    return false;
+},
+
+/**
+ * @method isPlaying
+ * @return {bool}
+ */
+isPlaying : function (
+)
+{
+    return false;
+},
+
+/**
+ * @method setLooping
+ * @param {bool} arg0
+ */
+setLooping : function (
+bool 
+)
+{
+},
+
+/**
+ * @method setUserInputEnabled
+ * @param {bool} arg0
+ */
+setUserInputEnabled : function (
+bool 
+)
+{
+},
+
+/**
+ * @method create
+ * @return {cc.experimental::ui::VideoPlayer}
+ */
+create : function (
+)
+{
+    return cc.experimental::ui::VideoPlayer;
+},
+
+/**
+ * @method VideoPlayer
+ * @constructor
+ */
+VideoPlayer : function (
+)
+{
+},
+
+};
